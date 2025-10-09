629f4c20be309ff17333fefdbd13770fb02ed455
wirsindesgeyers <kauanmaiagomes1307@gmail.com>
2025-10-09 15:59:16 -0400

    Implementados os endpoints de Criar, deletar nota, ver nota, ver todas as notas e seus respectivos meétodos no noteServices.

--- src/main/java/com/kauan/br/projetinhoteste/controller/NotesController.java
+++ src/main/java/com/kauan/br/projetinhoteste/controller/NotesController.java
@@ -1,13 +1,15 @@
 package com.kauan.br.projetinhoteste.controller;
 
 
+import com.kauan.br.projetinhoteste.model.Note;
 import com.kauan.br.projetinhoteste.services.NoteServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping ("/notes")
@@ -16,11 +18,49 @@ public class NotesController {
     @Autowired
     private NoteServices noteServices;
 
-    //pega a anotação pelo id
-    @GetMapping("/{id}")
-    public ResponseEntity findNoteById(Long id){
-        noteServices.getNoteById(id);
-        return ResponseEntity.status(HttpStatus.OK).body("Notas encontradas com sucesso.");
+    //cria a anotação
+    @PostMapping
+    public ResponseEntity CreateNote(@RequestBody Note newNote){
+
+        Note savedNote = noteServices.createNote(newNote);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedNote);
     }
 
+
+
+    //pega a anotação pelo id
+    @GetMapping("/{id}")
+    public ResponseEntity<Note> findNoteById(@PathVariable Long id) {
+        Optional<Note> note = noteServices.getNoteById(id);
+
+        if (note.isPresent()) {
+            return ResponseEntity.ok(note.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Note> deleteNoteById(@PathVariable Long id){
+        Optional<Note> note;
+        if(noteServices.getNoteById(id).isPresent()){
+            noteServices.deleteNote(id);
+            return ResponseEntity.noContent().build();
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+
+    //pegar todas as notas
+    @GetMapping
+    public ResponseEntity<List<Note>> getAllNotes(){
+        List<Note> note = noteServices.getAllNotes();
+        
+        return ResponseEntity.ok().body(note);
+    }
+
+
 }
+
+
